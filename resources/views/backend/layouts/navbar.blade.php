03236e7cdc809bc92642de3e3cad0ac0f07c748d
JulzPerez <julietoperez@gmail.com>
2022-03-27 10:50:23 -0700

    changes-mar27-1

--- resources/views/backend/layouts/navbar.blade.php
+++ resources/views/backend/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Online Examination</title>
+        <title>Online Quiz App</title>
         <link type="text/css" href="{{asset('edmin/code/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
         <link type="text/css" href="{{asset('edmin/code/bootstrap/css/bootstrap-responsive.min.css')}}" rel="stylesheet">
         <link type="text/css" href="{{asset('edmin/code/css/theme.css')}}" rel="stylesheet">
@@ -17,12 +17,12 @@
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="/">Online Examination </a>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="/">Online Quiz App </a>
                     <div class="nav-collapse collapse navbar-inverse-collapse">
                         <ul class="nav nav-icons">
-                            <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
+                            <!-- <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
                             <li><a href="#"><i class="icon-eye-open"></i></a></li>
-                            <li><a href="#"><i class="icon-bar-chart"></i></a></li>
+                            <li><a href="#"><i class="icon-bar-chart"></i></a></li> -->
                         </ul>
                         <form class="navbar-search pull-left input-append" action="#">
                         <input type="text" class="span3">
@@ -31,7 +31,7 @@
                         </button>
                         </form>
                         <ul class="nav pull-right">
-                            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
+                            <!-- <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
                                 <b class="caret"></b></a>
                                 <ul class="dropdown-menu">
                                     <li><a href="#">Item No. 1</a></li>
@@ -40,17 +40,35 @@
                                     <li class="nav-header">Example Header</li>
                                     <li><a href="#">A Separated link</a></li>
                                 </ul>
-                            </li>
-                            <li><a href="#">Support </a></li>
-                            <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            </li> -->
+                            <!-- <li><a href="#">Support </a></li> -->
+                            <li class="nav-user dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 <img src="{{asset('edmin/code/images/user.png')}}" class="nav-avatar" />
-                                <b class="caret"></b></a>
+                                <b class="caret"></b>
+                                </a>
                                 <ul class="dropdown-menu">
-                                    <li><a href="#">Your Profile</a></li>
+                                    <!-- <li><a href="#">Your Profile</a></li>
                                     <li><a href="#">Edit Profile</a></li>
-                                    <li><a href="#">Account Settings</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Logout</a></li>
+                                    <li><a href="#">Account Settings</a></li> 
+                                    <li class="divider"></li> -->
+                                    <li> 
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                        <button class="dropdown-item" type="button">Action</button>
+                                        <button class="dropdown-item" type="button">Another action</button>
+                                        <button class="dropdown-item" type="button">Something else here</button>
+                                    </div>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                       <i class="icon-inbox"></i> {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                    
+                                    </li>
                                 </ul>
                             </li>
                         </ul>
