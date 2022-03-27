97020da4123cf340ccf4b4fe3896732cbfecd7ce
JulzPerez <julietoperez@gmail.com>
2022-03-27 10:59:53 -0700

    changes-mar27-3

--- app/Http/Controllers/QuizCOntroller.php
+++ app/Http/Controllers/QuizCOntroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Quiz;
-class QuizCOntroller extends Controller
+class QuizController extends Controller
 {
     /**
      * Display a listing of the resource.
